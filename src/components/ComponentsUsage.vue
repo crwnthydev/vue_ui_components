49b28e46b6b84f5f71c4f7770cfa0344cfabbd1e
hdn555 <ohladitetrahanie@gmail.com>
2022-05-26 20:38:46 +0500

    input

--- src/components/ComponentsUsage.vue
+++ src/components/ComponentsUsage.vue
@@ -1,11 +1,23 @@
 <template>
 	<div class="demos">
-		<my-input></my-input>
+		<div class="demos__input">
+			<span class="demos__tip">Input</span>
+			<my-input type="text" placeholder="Text-input" v-model="inputModel"></my-input>
+			<p class="demos__result">{{inputModel}}</p>
+		</div>
+		
 	</div>
 </template>
 
 <script>
 
+export default {
+	data(){
+		return {
+			inputModel: ''
+		}	
+	}
+}
 </script>
 
 <style lang="scss">
