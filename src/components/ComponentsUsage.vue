37c4cfea77f4b4d8032f538a899b2ceb5d8bf45b
hdn555 <ohladitetrahanie@gmail.com>
2022-06-05 12:34:21 +0500

    select / demos style

--- src/components/ComponentsUsage.vue
+++ src/components/ComponentsUsage.vue
@@ -1,9 +1,25 @@
 <template>
 	<div class="demos">
-		<div class="demos__input">
-			<span class="demos__tip">Input</span>
-			<my-input type="text" placeholder="Text-input" v-model="inputModel"></my-input>
-			<p class="demos__result">{{inputModel}}</p>
+		<div class="demos__input demo">
+			<div class="demo__active-element">
+				<span class="demo__tip">Input</span>
+				<my-input type="text" placeholder="Text-input" v-model="inputModel"></my-input>
+			</div>
+			<div class="demo__result">
+				<span class="demo__tip">Input result:</span>
+				<p>{{inputModel}}</p>
+			</div>
+		</div>
+
+		<div class="demos__select demo">
+			<div class="demo__active-element">
+				<span class="demo__tip">Select</span>
+				<my-select v-model="selectModel" :options="selectOptions" :titleOption="selectTitle"/>
+			</div>
+			<div class="demo__result">
+				<span class="demo__tip">Select result:</span>
+				<p>{{selectModel}}</p>
+			</div>
 		</div>
 		
 	</div>
@@ -14,12 +30,69 @@
 export default {
 	data(){
 		return {
-			inputModel: ''
+			inputModel: '',
+			selectModel: '',
+			selectOptions: [
+				{value: 'Demo option 1', name: 'Demo option 1'},
+				{value: 'Demo option 2', name: 'Demo option 2'},
+				{value: 'Demo option 3', name: 'Demo option 3'},
+				{value: 'Demo option 4', name: 'Demo option 4'},
+			],
+			selectTitle: "Choose from list",
 		}	
 	}
 }
 </script>
 
 <style lang="scss">
+
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	.demos {
+		margin: 30px 0 0 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-size: 16px;
+		line-height: 1.3em;
+		color: #000000;
+
+		input,
+		select {
+			min-width: 150px;
+		}
+	}
+
+	.demo {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 0 0 30px 0;
+
+		&__tip {
+			background-color: rgba(47, 187, 112, 0.7);
+			margin: 0 0 7px 0;
+			min-width: 150px;
+		}
+
+		&__active-element {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			margin: 0 0 20px 0;
+		}
+
+		&__result {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			min-width: 150px;
+			border-bottom: 1px solid black;
+		}
+	}
 
 </style>
